a6ef1c76b3adff93656269a6b67199d61c532db4
gtillman1 <143618693+gtillman1@users.noreply.github.com>
2024-05-13 13:30:24 +0000

    Movies

--- script.js
+++ script.js
@@ -4,4 +4,35 @@
 
 // Submit Button 
 let submitButton = document.querySelector("button");
-submitButton.addEventListener("click", function() {});
+ submitButton.addEventListener("click", function() {
+
+let movieInput = document.querySelector("Input").value;
+let comedy= ["Madea.jpg", "Friday.jpg","Rush.jpg"]
+let action= ["Madea.jpg", "Friday.jpg","Rush.jpg"]
+let drama= ["Madea.jpg", "Friday.jpg","Rush.jpg"]
+let results = document.querySelector("div")
+
+    if (
+    "comedy" === movieInput)
+        for (let e of comedy){
+let img = document.createElement("img")
+img.src = e
+results.appendChild(img)
+        }
+        if (
+            "action" === movieInput)
+                for (let e of comedy){
+        let img = document.createElement("img")
+        img.src = e
+        results.appendChild(img)
+                }
+                if (
+                    "drama" === movieInput)
+                        for (let e of comedy){
+                let img = document.createElement("img")
+                img.src = e
+                results.appendChild(img)
+                        }
+         
+
+    });
